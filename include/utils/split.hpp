483160168dde77fc7559eeb9ece8fb7acd9325e9
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-01-31 23:26:47 +0900

    first commit

---
+++ include/utils/split.hpp
@@ -0,0 +1,48 @@
+#ifndef IS_UTILS_SPLIT_HPP
+#define IS_UTILS_SPLIT_HPP
+
+#include <string>
+#include <vector>
+
+namespace Is
+{
+    namespace utils
+    {
+        /**
+         * @brief split関数
+         * https://marycore.jp/prog/cpp/std-string-split/
+         * @param msg 
+         * @param sep 
+         * @return std::vector<std::string> 
+         */
+        std::vector<std::string> split(const std::string& msg, const std::string& sep)
+        {
+            int pos = 0;
+            int len_sep = sep.length();
+            std::vector<std::string> tokens;
+
+            if (len_sep == 0)
+            {
+                tokens.push_back(msg);
+            }
+            else
+            {
+                auto offset = std::string::size_type(0);
+                while (true)
+                {
+                    auto pos = msg.find(sep, offset);
+                    if (pos == std::string::npos)
+                    {
+                        tokens.push_back(msg.substr(offset));
+                        break;
+                    }
+                    tokens.push_back(msg.substr(offset, pos - offset));
+                    offset = pos + len_sep;
+                }
+            }
+
+            return tokens;
+        }
+    } // utils
+}
+#endif
