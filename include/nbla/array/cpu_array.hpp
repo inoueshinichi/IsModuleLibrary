e0e43d52ea3f7ebb45ea263ea955648c4a0e5975
Inoue Shinichi for Windows Desktop <inoue.shinichi.1800@gmail.com>
2021-02-21 01:18:49 +0900

    init

---
+++ include/nbla/array/cpu_array.hpp
@@ -0,0 +1,42 @@
+#ifndef IS_NBLA_CPU_ARRAY_HPP
+#define IS_NBLA_CPU_ARRAY_HPP
+
+#include "nbla/array.hpp"
+#include "nbla/memory/allocator.hpp"
+
+namespace Is
+{
+    namespace nbla
+    {
+        /**
+         * @brief CPUデバイス用の配列
+         * 
+         */
+        class NBLA_API CpuArray : public Array
+        {
+        public:
+            CpuArray(const Size_t size, dtypes dtype, const Context& ctx);
+            CpuArray(const Size_t size, dtypes dtype, const Context& ctx, AllocatorMemory&& mem);
+            virtual ~CpuArray();
+            virtual void copy_from(const Array* src_array); // NBLA_DEFINE_FUNC_COPY_FROMマクロで実装
+            virtual void zero();
+            virtual void fill(float value); // NBLA_DEFINE_FUNC_FILLマクロで実装
+            static Context filter_context(const Context& ctx);
+        };
+
+
+        /**
+         * @brief キャッシュ機能を搭載したCPUデバイス用の配列
+         * 
+         */
+        class NBLA_API CpuCachedArray : public CpuArray
+        {
+        public:
+            explicit CpuCachedArray(const Size_t size, dtypes dtype, const Context& ctx); // explicitはdtypesの暗黙の型変換を禁止する？
+            virtual ~CpuCachedArray();
+            static Context filter_context(const Context& ctx);
+        };
+
+    }
+}
+#endif
