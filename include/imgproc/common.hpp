50123a84c24c1d3cee0af728ce318980c4fc9ecd
Inoue Shinichi for Windows Desktop <inoue.shinichi.1800@gmail.com>
2021-03-02 13:02:12 +0900

    imgproc defs and common

---
+++ include/imgproc/common.hpp
@@ -0,0 +1,20 @@
+#ifndef IS_IMGPROC_COMMON_HPP
+#define IS_IMGPROC_COMMON_HPP
+#include "imgproc/defs.hpp"
+#include "utils/split.hpp"
+#include "utils/format_string.hpp"
+
+#include <memory>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <cstring> //std::memset
+
+namespace Is
+{
+    namespace imgproc
+    {
+
+    }
+}
+#endif
---
+++ include/imgproc/defs.hpp
@@ -0,0 +1,19 @@
+#ifndef IS_IMGPROC_DEFS_HPP
+#define IS_IMGPROC_DEFS_HPP
+
+#ifdef _MSC_VER
+    // https://qiita.com/Chironian/items/462a3bdf271d5f0b00b6#%EF%BC%92%EF%BC%93c4251%E8%AD%A6%E5%91%8A%E3%81%8C%E5%87%BA%E3%82%8B
+    #pragma warning(disable:4251)
+#endif
+
+#if defined(_MSC_VER) && !defined(__CUDACC__)
+    #if defined(is_imgproc_EXPORTS) || defined(is_imgproc_dbg_EXPORTS)
+        #define IMGPROC_API __declspec(dllexport)
+    #else
+        #define IMGPROC_API __declspec(dllimport)
+    #endif
+#else
+    #define IMGPROC_API
+#endif
+
+#endif
