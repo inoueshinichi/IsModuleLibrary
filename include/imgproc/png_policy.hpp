6f01a44edc6a994855b97a07ea8a1853d18e7269
Inoue Shinichi for Windows Desktop <inoue.shinichi.1800@gmail.com>
2021-03-24 19:15:59 +0900

    define some func

--- include/imgproc/png_policy.hpp
+++ include/imgproc/png_policy.hpp
@@ -2,6 +2,7 @@
 #define IS_IMGPROC_PNG_POLICY_HPP
 #include "imgproc/common.hpp"
 
+#include <cstdint>
 
 namespace Is
 {
@@ -15,8 +16,18 @@ namespace Is
             class IMGPROC_API PngFilePolicy
             {
             public:
-                void save(const string& filename, byte* data, int32_t width, int32_t height, int channels);
-                void load(const string& filename, byte* data, int32_t width, int32_t height, int channels);
+                PngFilePolicy();
+                ~PngFilePolicy();
+                PngFilePolicy(const PngFilePolicy&) = delete;
+                PngFilePolicy& operator=(const PngFilePolicy&) = delete;
+                PngFilePolicy(PngFilePolicy&&) = default;
+                PngFilePolicy& operator=(PngFilePolicy&&) = default;
+
+                void set_data(byte* data, int insert_color = -1);
+                void get_data(byte* data, int extract_color = -1);
+                void save(const string& filename, byte* data, int32_t width, int32_t height, int32_t channels, bool is_dump);
+                void load(const string& filename, int32_t& width, int32_t& height, int32_t& channels, bool is_dump);
+                void dump() const;
             };
         } // namespace format_policy
     } // namespace imgproc
