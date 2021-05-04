035efdf1f2bb7aa7c79d38a00e5c1b46d200197c
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-05-04 22:20:15 +0900

    change to IMGPROC_API, add INGPROC_ALING

--- include/imgproc/defs.hpp
+++ include/imgproc/defs.hpp
@@ -1,5 +1,4 @@
-#ifndef IS_IMGPROC_DEFS_HPP
-#define IS_IMGPROC_DEFS_HPP
+#pragma once
 
 #ifdef _MSC_VER
     // https://qiita.com/Chironian/items/462a3bdf271d5f0b00b6#%EF%BC%92%EF%BC%93c4251%E8%AD%A6%E5%91%8A%E3%81%8C%E5%87%BA%E3%82%8B
@@ -8,12 +7,15 @@
 
 #if defined(_MSC_VER) && !defined(__CUDACC__)
     #if defined(is_imgproc_EXPORTS) || defined(is_imgproc_dbg_EXPORTS)
-        #define IS_IMGPROC_API __declspec(dllexport)
+        #define IMGPROC_API __declspec(dllexport)
     #else
-        #define IS_IMGPROC_API __declspec(dllimport)
+        #define IMGPROC_API __declspec(dllimport)
     #endif
 #else
-    #define IS_IMGPROC_API
+    #define IMGPROC_API
 #endif
 
-#endif
+
+// C++11 gives alignas as standard
+// http://en.cppreference.com/w/cpp/language/alignas
+#define IMGPROC_ALIGN(N) alignas(N)
