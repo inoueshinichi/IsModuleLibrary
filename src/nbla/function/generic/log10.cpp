b8260180f1f72cdc84a1b8d58e65c5f548284e85
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-05-06 01:34:40 +0900

    first commit

---
+++ src/nbla/function/generic/log10.cpp
@@ -0,0 +1,9 @@
+#include <nbla/function/log10.hpp>
+
+namespace Is
+{
+    namespace nbla
+    {
+        NBLA_REGISTER_FUNCTION_SOURCE(Log10)
+    }
+}
---
+++ src/nbla/function/generic/log1p.cpp
@@ -0,0 +1,9 @@
+#include <nbla/function/log1p.hpp>
+
+namespace Is
+{
+    namespace nbla
+    {
+        NBLA_REGISTER_FUNCTION_SOURCE(Log1p)
+    }
+}
