2465e73e94352a26cffa3b7e9db47ac2033eacbd
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-05-23 02:19:40 +0900

    concatenate

---
+++ src/nbla/function/generic/concatenate.cpp
@@ -0,0 +1,98 @@
+#include <nbla/array.hpp>
+#include <nbla/function/concatenate.hpp>
+#include <nbla/nd_array.hpp>
+
+namespace Is
+{
+    namespace nbla
+    {
+        NBLA_REGISTER_FUNCTION_SOURCE(Concatenate, int)
+
+
+        template <typename T>
+        void Concatenate<T>::setup_impl(const NdArrays& inputs, const NdArrays& outputs)
+        {
+            Shape_t shape = inputs[0]->shape();
+            if (axis_ < 0)
+                axis_ += shape.size(); // 最下位の次元で連結
+            
+            NBLA_CHECK(axis_ >= 0, 
+                       error_code::value,
+                       "axis must not be less than zero, got %d", 
+                       axis_);
+
+            auto axis = static_cast<Shape_t::size_type>(this->axis_);
+
+            NBLA_CHECK(axis <= shape.size(), 
+                       error_code::value,
+                       "axis must be less than or equal to ndim of input. "
+                       "axis: %d > ndim of input: %d.",
+                       axis_, 
+                       shape.size());
+
+
+            inner_total_size_ = 0;
+            for (NdArrays::size_type i = 0; i < inputs.size(); ++i)
+            {
+                // 連結する多次元配列の次元が有効かチェック
+                NBLA_CHECK(inputs[i]->shape().size() != 0, 
+                           error_code::value,
+                           "input value(inputs[%d]) does not exist. "
+                           "inputs[%d]->shape().size(): %d.",
+                           i,
+                           i, 
+                           inputs[i]->shape().size());
+            
+                const int inner_size = inputs[i]->size(this->axis_);
+                inner_total_size_ += inner_size;
+
+                if (i >= 1) 
+                {
+                    shape[axis_] += inputs[i]->shape()[axis_]; // Accumulate size along axis
+                    for (Shape_t::size_type j = 0; j < shape.size(); j++) 
+                    {
+                        if (j != axis) 
+                        {
+                            NBLA_CHECK(inputs[i]->shape()[j] == shape[j], 
+                                       error_code::value,
+                                       "Dimensions of inputs must match. "
+                                       "inputs[%d]->shape()[%d]: %d != "
+                                       "inputs[0]->shape()[%d]: %d.",
+                                       i, 
+                                       j, 
+                                       inputs[i]->shape()[j], 
+                                       j, 
+                                       shape[j]);
+                        }
+                    }
+                }
+            } // for (NdArrays::size_type i = 0; i < inputs.size(); ++i)
+
+            outputs[0]->reshape(shape, true);
+            outer_size_ = inputs[0]->size() / inputs[0]->size(axis_);
+        }
+
+
+        template <typename T>
+        void execute_impl(const NdArrays& inputs, const NdArrays& outputs)
+        {
+            T* y = outputs[0]->cast_data_and_get_pointer<T>(this->ctx_, true);
+            int inner_offset = 0;
+            for (NdArrays::size_type c = 0; c < inputs.size(); ++c) 
+            {
+                const T* x = inputs[c]->get_data_pointer<T>(this->ctx_);
+                const int inner_size = inputs[c]->size(this->axis_);
+
+                for (int o = 0; o < outer_size_; ++o) 
+                {
+                    for (int i = 0; i < inner_size; ++i) 
+                    {
+                        y[o * inner_total_size_ + inner_offset + i] = x[o * inner_size + i];
+                    }
+                }
+                inner_offset += inner_size;
+            }
+        }
+
+    }
+}
