3b0a68db160c178741016a4e8eab4a89c6cf3722
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-05-24 00:41:45 +0900

    slice

---
+++ include/nbla/function/slice.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <nbla/cpu.hpp>
+#include <nbla/function.hpp>
+#include <nbla/function_registry.hpp>
+
+namespace Is
+{
+    namespace nbla
+    {
+        NBLA_REGISTER_FUNCTION_HEADER(Slice, const vector<int>&, const vector<int>&, const vector<int>&)   
+        // start
+        // stop
+        // step
+
+        /**
+         * @brief Slice arrays along specified axis.
+         * 
+         * Inputs:
+         * - N-D array.
+         * 
+         * Outputs:
+         * - M-D array.
+         * 
+         * Slice input tensor.
+         * y = x[start[0]:stop[0]:step[0], start[1]:stop[1]:step[1], ...]
+         * 
+         * @param T Data type for computation.
+         */
+        template <typename T>
+        class Slice : public BaseFunction<const vector<int>&, const vector<int>&, const vector<int>&>
+        {
+        protected:
+            // These settings are array to realize different slice amount for each data.
+            vector<vector<int>> start_;
+            vector<vector<int>> stop_;
+            vector<vector<int>> step_;
+
+            int base_axis_;
+
+            // SPECIAL condition
+            enum { SLICE_NONE = 0x7fffffff };
+
+        public:
+            Slice(const Context& ctx, const vector<int>& start, const vector<int>& stop, const vector<int>& step)
+                : BaseFunction(ctx, start, stop, step)
+                , start_(1)
+                , stop_(1)
+                , step_(1)
+                , base_axis_(0)
+            {
+                start_[0] = start;
+                stop_[0] = stop;
+                step_[0] = step;
+            }
+
+
+            virtual ~Slice() {}
+            
+
+            virtual shared_ptr<Function> copy() const 
+            {
+                return create_Slice(ctx_, start_[0], stop_[0], step_[0]);
+            }
+
+
+            virtual vector<dtypes> in_types() 
+            {
+                return vector<dtypes>{get_dtype<T>(), get_dtype<T>()};
+            }
+
+
+            virtual vector<dtypes> out_types() { return vector<dtypes>{get_dtype<T>()}; }
+            virtual int min_inputs() { return 1; }
+            virtual int min_outputs() { return 1; }
+            virtual string name() { return "Slice"; }
+            virtual vector<string> allowed_array_classes() 
+            {
+                return SingletonManager::get<Cpu>()->array_classes();
+            }
+
+
+        protected:
+            NBLA_API virtual void setup_impl(const NdArrays& inputs,
+                                             const NdArrays& outputs);
+            NBLA_API virtual void execute_impl(const NdArrays& inputs,
+                                               const NdArrays& outputs);
+
+            //NBLA_API bool skip_check(const NdArrays& outputs); // ver19.0に実装がない
+        
+        private:
+            NBLA_API void slice_excute_recursive(const NdArray* inp, NdArray* outp,
+                                                  const T* x, T* y, 
+                                                  int x_offset, int y_offset, 
+                                                  int dim, int& slice_index);
+        };
+    }
+}
