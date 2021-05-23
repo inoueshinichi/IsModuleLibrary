79b2472f3c65e18e76d10dee46176dac1898f412
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-05-24 00:44:59 +0900

    == <= < > >= >

---
+++ src/nbla/function/generic/greater_equal.cpp
@@ -0,0 +1,9 @@
+#include <nbla/function/greater_equal.hpp>
+
+namespace Is
+{
+    namespace nbla
+    {
+        NBLA_REGISTER_FUNCTION_SOURCE(GreaterEqual)
+    }
+}
---
+++ src/nbla/function/generic/less.cpp
@@ -0,0 +1,9 @@
+#include <nbla/function/less.hpp>
+
+namespace Is
+{
+    namespace nbla
+    {
+        NBLA_REGISTER_FUNCTION_SOURCE(Less)
+    }
+}
