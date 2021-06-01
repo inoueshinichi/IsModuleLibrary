a787f2ceec23b20dac00532631ab037d00c1ab13
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-06-02 01:08:23 +0900

    add test

--- test/test_nnabla/test_nnabla.cpp
+++ test/test_nnabla/test_nnabla.cpp
@@ -11,7 +11,10 @@
 #include <nbla/synced_array.hpp>
 #include <nbla/nd_array.hpp>
 
+#include <nbla/nd_array_extra.hpp>
+
 // nnabla func
+#include <nbla/function/add_scalar.hpp>
 #include <nbla/function/randn.hpp>
 
 
@@ -52,6 +55,81 @@ void show_status_memory(Is::nbla::Memory* memory)
     std::cout << "---------------------------" << std::endl;
 }
 
+template <typename T>
+void show_ndarray_contents(const Is::nbla::Context &ctx, Is::nbla::NdArrayPtr ndarray)
+{
+    using namespace Is::nbla;
+    Shape_t shape = ndarray->shape();
+    Stride_t strides = ndarray->strides();
+    Size_t size = ndarray->size();
+    Size_t ndim = ndarray->ndim();
+    auto synced_array = ndarray->array();
+    const T *data = static_cast<const T *>(synced_array->data_ptr(get_dtype<T>(), ctx));
+
+    std::cout << "[";
+    for (size_t c = 0; c < shape[0]; ++c)
+    {
+        std::cout << "[";
+        for (size_t j = 0; j < shape[1]; ++j)
+        {
+            if (j != 0)
+                std::cout << "  ";
+            std::cout << "[";
+            for (size_t i = 0; i < shape[2]; ++i)
+            {
+                T tmp = data[c * strides[0] + j * strides[1] + i * strides[2]];
+                std::cout << tmp;
+                if (i != shape[2] - 1)
+                    std::cout << " ";
+            }
+
+            if (j == shape[1] - 1)
+            {
+                if (c != shape[0] - 1)
+                    std::cout << "]]" << std::endl;
+                else
+                    std::cout << "]]";
+            }
+            else
+                std::cout << "]" << std::endl;
+        }
+    }
+    std::cout << "]" << std::endl;
+}
+
+
+template <typename T>
+void show_2d_array(const Is::nbla::Context& ctx, Is::nbla::NdArrayPtr ndarray)
+{
+    using namespace Is::nbla;
+    Shape_t shape = ndarray->shape();
+    Stride_t strides = ndarray->strides();
+    Size_t size = ndarray->size();
+    Size_t ndim = ndarray->ndim();
+    auto synced_array = ndarray->array();
+    const T *data = static_cast<const T *>(synced_array->data_ptr(get_dtype<T>(), ctx));
+
+    std::cout << "[";
+    for (int i = 0; i < shape[0]; ++i)
+    {
+        std::cout << "[";
+        for (int j = 0; j < shape[1]; ++j)
+        {
+            T tmp = data[i * strides[0] + j * strides[1]];
+            std::cout << tmp;
+
+            if (j != shape[1] - 1)
+                std::cout << "  ";
+        }
+        std::cout << "]";
+
+        if (i != shape[0] - 1)
+            std::cout << std::endl;
+    }
+    std::cout << "]" << std::endl;
+}
+
+// ---------------------------------------------------------------------------
 
 namespace
 {
@@ -95,6 +173,188 @@ namespace
         show_status_memory(sub_mem.get());
         printf("Memory ->\n");
         show_status_memory(&mem);
+    }
+
+
+    TEST(nnabla_core, allocator)
+    {
+        using namespace Is::nbla;
+        using memory_type = CpuMemory;
+        string device_id{ "cpu" };
+        size_t bytes{ 64 };
+
+        AllocatorMemory alloc_mem;
+        std::shared_ptr<NaiveAllocator<memory_type>> 
+            allocator(new NaiveAllocator<memory_type>());
+        
+        alloc_mem = allocator->alloc(bytes, device_id);
+    }
+
+
+    TEST(nnabla_core, caching_allocator)
+    {
+        using namespace Is::nbla;
+        using memory_type = CpuMemory;
+        string device_id{ "cpu" };
+
+        std::shared_ptr<CachingAllocatorWithBuckets<memory_type>> 
+            allocator = std::make_shared<CachingAllocatorWithBuckets<memory_type>>();
+
+        // 初めてのメモリ生成(小)では, 内部で1MBのメモリプールが生成されて、512B単位で必要な容量だけ切り崩している
+        // 初めてのメモリ生成(大)では, 内部で生成したメモリ数だけ確保する
+        AllocatorMemory alloc_mem_small_1 = allocator->alloc(64, device_id);
+        AllocatorMemory alloc_mem_small_2 = allocator->alloc(530, device_id);
+        AllocatorMemory alloc_mem_large_1 = allocator->alloc((1 << 20) + 100, device_id);
+
+        // キャッシュされているメモリを表示
+        std::cout << "Device_Memory_Used_In_Bytes: " 
+                << allocator->device_memory_used_in_bytes(device_id)
+                << std::endl;
+        std::cout << std::endl;
+
+        // キャシュされている不要なメモリを削除
+        allocator->free_unused_device_caches(device_id);
+
+        // キャッシュされているメモリを表示
+        std::cout << "Device_Memory_Used_In_Bytes: "
+                << allocator->device_memory_used_in_bytes(device_id)
+                << std::endl;
+        std::cout << std::endl;
+
+        // alloc_mem_small_2に対応するMemoryを破棄
+        allocator->free(alloc_mem_small_2.memory());
+
+        // キャッシュされているメモリを表示
+        std::cout << "Device_Memory_Used_In_Bytes: "
+                << allocator->device_memory_used_in_bytes(device_id)
+                << std::endl;
+    }
+
+
+    TEST(nnabla_core, array)
+    {
+        using namespace Is::nbla;
+        using byte = unsigned char;
+        string device_id{"cpu"};
+        Context ctx_cpu({"cpu:float"}, "CpuArray", "0");
+        Size_t array_size = 512;
+
+        shared_ptr<Array> cpu_array_1 = shared_ptr<Array>(
+            ArrayCreator::create(array_size, dtypes::FLOAT, ctx_cpu));
+
+        std::printf("cpu_array_1: pointer -> %p\n", cpu_array_1->pointer<float>());
+        std::printf("cpu_array_1: const_pointer -> %p\n", cpu_array_1->const_pointer<float>());
+        std::printf("cpu_array_1: dtype -> %d\n", cpu_array_1->dtype());
+        std::printf("cpu_array_1: size -> %d\n", cpu_array_1->size());
+        std::cout << "cpu_array_1: context -> " << cpu_array_1->context().to_string() << std::endl;
+
+        Context ctx_cpu_cache({"cpu:float"}, "CpuCachedArray", "0");
+        shared_ptr<Array> cpu_cached_array_1 = shared_ptr<Array>(
+            ArrayCreator::create(array_size, dtypes::FLOAT, ctx_cpu_cache));
+
+        std::cout << "cpu_cached_array_1: context -> " << cpu_cached_array_1->context().to_string() << std::endl;
+
+        cpu_cached_array_1->zero();
+    }
+
+
+    TEST(nnabla_core, synced_array)
+    {
+        using namespace Is::nbla;
+        using byte = unsigned char;
+        string device_id{"cpu"};
+        Context ctx_cpu({"cpu:float"}, "CpuArray", "0");
+        Size_t array_size = 512;
+
+        auto sync_array_1 = std::make_shared<SyncedArray>(array_size);
+        sync_array_1->zero();
+        sync_array_1->cast(dtypes::UBYTE, ctx_cpu);
+        std::cout << "sync_array_1: head_array_class -> " << sync_array_1->head_array_class() << std::endl;
+        std::cout << "sync_array_1: get_num_arrays -> " << sync_array_1->get_num_arrays() << std::endl;
+        std::cout << "sync_array_1: size -> " << sync_array_1->size() << std::endl;
+        std::cout << "sync_array_1: modification_count -> " << sync_array_1->modification_count() << std::endl;
+    }
+
+
+    TEST(nnabla_core, ndarray)
+    {
+        using namespace Is::nbla;
+        using byte = unsigned char;
+        string device_id{"cpu"};
+        Context ctx_cpu({"cpu:float"}, "CpuArray", "0");
+        Size_t array_size = 512;
+
+        auto ndarr_1 = NdArray::create(Shape_t{3, 16, 16});
+        ndarr_1->fill(128);
+        ndarr_1->cast(dtypes::DOUBLE, ctx_cpu);
+        show_ndarray_contents<double>(ctx_cpu, ndarr_1);
+
+        std::cout << " test zeros ---------------" << std::endl;
+
+        auto ndarray_zeros = zeros<byte>(ctx_cpu, Shape_t{3, 16, 16});
+        show_ndarray_contents<double>(ctx_cpu, ndarray_zeros);
+
+        std::cout << " test ones ---------------" << std::endl;
+
+        auto ndarray_ones = ones<byte>(ctx_cpu, Shape_t{3, 16, 16});
+        show_ndarray_contents<double>(ctx_cpu, ndarray_ones);
+
+        std::cout << " test eye ---------------" << std::endl;
+
+        auto ndarray_eye = eye<byte>(ctx_cpu, 3, 5);
+        show_2d_array<double>(ctx_cpu, ndarray_eye);
+
+        auto ndarray_eye2 = eye<byte>(ctx_cpu, 5, 3);
+        show_2d_array<double>(ctx_cpu, ndarray_eye2);
+    }
+
+
+    TEST(nnabla_func, randn)
+    {
+        using namespace Is::nbla;
+        using byte = unsigned char;
+        string device_id{"cpu"};
+        Context ctx_cpu({"cpu:float"}, "CpuArray", "0");
+
+        float mu = 0.0;
+        float sigma = 1.0;
+        int seed = 2021;
+        Shape_t shape = {3, 5, 5};
+        // auto randn_ptr = Randn<float>(ctx_cpu, mu, sigma, shape, seed).copy();
+        // auto out_ndarray = NdArray::create();
+        // std::vector<NdArrayPtr> inputs;
+        // std::vector<NdArrayPtr> outputs{ out_ndarray };
+        // randn_ptr->setup(inputs, outputs);
+        // randn_ptr->execute(inputs, outputs);
+        auto ndarray_out_d = randn<double>(ctx_cpu, shape, mu, sigma, seed);
+        show_ndarray_contents<double>(ctx_cpu, ndarray_out_d);
+        std::cout << ndarray_out_d->get_data_pointer<double>(ctx_cpu)[0] << std::endl;
+
+        auto ndarray_out_f = randn<float>(ctx_cpu, shape, mu, sigma, seed);
+        show_ndarray_contents<float>(ctx_cpu, ndarray_out_f);
+        std::cout << ndarray_out_f->get_data_pointer<float>(ctx_cpu)[0] << std::endl;
+    }
+
+
+    TEST(nnabla_func, add_scalar)
+    {
+        using namespace Is::nbla;
+       
+        string device_id{"cpu"};
+        Context ctx_cpu({"cpu:float"}, "CpuArray", "0");
+
+        // zero
+        auto ndarray_zeros = zeros<float>(ctx_cpu, Shape_t{3, 16, 16});
+        show_ndarray_contents<float>(ctx_cpu, ndarray_zeros);
+
+        AddScalar<float> add_scalar(ctx_cpu, 15, false);
+        auto out_ndarray = NdArray::create();
+        std::vector<NdArrayPtr> inputs{ ndarray_zeros };
+        std::vector<NdArrayPtr> outputs{ out_ndarray };
+        add_scalar.setup(inputs, outputs);
+        add_scalar.execute(inputs, outputs);
+
+        show_ndarray_contents<float>(ctx_cpu, out_ndarray);
     }
 }
 
