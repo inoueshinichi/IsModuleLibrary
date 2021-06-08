b6009a76f808fa76cd52b12b2f9ac45c83c997d6
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-06-08 23:06:14 +0900

    add IMGPROC_CHECK_IMAGE_SHAPE macro

--- include/imgproc/common.hpp
+++ include/imgproc/common.hpp
@@ -1,13 +1,67 @@
 #pragma once
 
-#include "imgproc/defs.hpp"
-#include "utils/split.hpp"
-#include "utils/format_string.hpp"
+#include <imgproc/defs.hpp>
 
+/*NdArray*/
+#include <nbla/nd_array.hpp>
+#include <nbla/nd_array_extra.hpp>
+
+#include <utils/format_string.hpp>
+#include <utils/split.hpp>
+#include <utils/string_join.hpp>
+
+#include <stdexcept>
+#include <exception>
+#include <cassert>
+#include <cmath>
+#include <limits>
+#include <numeric>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
 #include <memory>
+#include <cstring>
 #include <string>
 #include <sstream>
-#include <exception>
-#include <stdexcept>
-#include <cstring> //std::memset
-#include <cassert>
+
+#include <tuple>
+#include <vector>
+#include <deque>
+#include <list>
+
+
+#define IMGPROC_CHECK_IMAGE_SHAPE(NDARRAY)                                        \
+    {                                                                             \
+        auto ndim = NDARRAY->ndim();                                              \
+        auto shape = NDARRAY->shape();                                            \
+        auto strides = NDARRAY->strides();                                        \
+        auto dtype = NDARRAY->dtype();                                            \
+                                                                                  \
+        if (nbla::dtypes::FLOAT != dtype)                                         \
+        {                                                                         \
+            throw std::runtime_error(                                             \
+                utils::format_string("Miss matching dtypes::FLOAT. Given is %s.", \
+                     nbla::dtype_to_string(dtype).c_str()));                      \
+        }                                                                         \
+                                                                                  \
+        if (ndim == 1 || ndim > 3)                                                \
+        {                                                                         \
+            throw std::runtime_error(                                             \
+                utils::format_string(                                             \
+                    "Miss matching 2 or 3 ndim. Given is %d",                     \
+                    ndim));                                                       \
+        }                                                                         \
+        if (ndim == 3)                                                            \
+        {                                                                         \
+            /*(C, H, W)*/                                                         \
+            int channels = shape[0];                                              \
+            if (channels > 3)                                                     \
+            {                                                                     \
+                throw std::runtime_error(                                         \
+                    utils::format_string(                                         \
+                        "Miss matching channels of top shape (<=3). Given is %d", \
+                        channels));                                               \
+            }                                                                     \
+        }                                                                         \
+    }
